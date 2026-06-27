abc051ea5e3c38602ed7811a8d60d36c68c9e0d9
Fahmyelmatry <fahmyelmatry2030@gmail.com>
2026-06-27 05:12:16 +0300

    Add GitHub Actions auto-deploy + fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Al-Rida-Company/',
   plugins: [
     react(),
     VitePWA({
